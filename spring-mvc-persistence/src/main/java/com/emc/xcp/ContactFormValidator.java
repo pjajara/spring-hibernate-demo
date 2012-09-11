93bc2f4670ff84e92ac75d070b29d84607157ed4
Pawankumar Jajara <pkjajara@gmail.com>
2012-09-11 12:19:45 -0700

    First Commit into Github!

---
+++ spring-mvc-persistence/src/main/java/com/emc/xcp/ContactFormValidator.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.emc.xcp;
+
+/**
+ * @author jajarp
+ *
+ */
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component("contactFormValidator")
+public class ContactFormValidator implements Validator
+{
+
+	@SuppressWarnings("unchecked")
+	public boolean supports(Class clazz) {
+		return Contact.class.isAssignableFrom(clazz);
+	}
+
+	
+	public void validate(Object model, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name",
+				"required.name", "Name is required.");
+	}
+}
