bd842064865faff03f9a027b22df50c6a462e4b1
Pawankumar Jajara <pkjajara@gmail.com>
2012-10-10 13:22:00 -0700

    More Changes to User Domain Object

--- spring-mvc-persistence/src/main/java/com/emc/xcp/domain/User.java
+++ spring-mvc-persistence/src/main/java/com/emc/xcp/domain/User.java
@@ -3,10 +3,82 @@
  */
 package com.emc.xcp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
 /**
  * @author jajarp
  *
  */
+@Entity
+@Table(name="USERS")
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column
+	private String firstname;
+	
+	@Column
+	private String lastname;
+	
+	@Column
+	private int age;
+	
+	public User(){
+		
+	}
+
+	public User(String firstname, String lastname, int age){
+		super();
+		this.age=age;
+		this.firstname=firstname;
+		this.lastname=lastname;
+	}
+	
+	@Override
+	public String toString(){
+		return ToStringBuilder.reflectionToString(this);
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 }
